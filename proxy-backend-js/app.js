07685a307957db1d31902ddb3416382248bc26d3
Lightsake-Sensei <805935229@qq.com>
2023-12-10 00:52:28 +0800

    express代理服务器完成

--- proxy-backend-js/app.js
+++ proxy-backend-js/app.js
@@ -1,36 +1,77 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
-const port = 3000;
+const port = 5000;
+const cors = require('cors');
+
+const { readYamlFile } = require("./utils/yaml.js"); // Import readYamlFile function
+
+
+
+// Replace this with your actual token retrieval logic
+const getTokenFromYaml = () => {
+    try {
+        const yamlData = readYamlFile("./config/token.yaml");
+
+        if (yamlData && yamlData.token) {
+            return yamlData.token;
+        } else {
+            console.error("Token not found in the YAML file.");
+            return null;
+        }
+    } catch (err) {
+        console.error("Error reading token.yaml:", err.message);
+        return null;
+    }
+};
+
+const token = getTokenFromYaml();
+if (!token) {
+    console.error("Unable to retrieve the token from token.yaml. Exiting.");
+    process.exit(1);
+}
+
+// Set the API key for dashscope
+bearerToken = token;
+app.use(cors());
 
 app.use(express.json());
 
-app.post('/generate-text', async (req, res) => {
-  try {
-    const apiUrl = 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation';
-    const requestData = {
-      model: 'qwen-v1',
-      input: {
-        prompt: '你好'
-      },
-      parameters: {}
-    };
+// app.use(cors({
+//   origin: 'http://localhost:8080',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }));
 
-    const response = await axios.post(apiUrl, requestData, {
-      headers: {
-        'Content-Type': 'application/json',
-        // 如果需要在请求头中添加其他信息，可以在这里添加
-      }
-    });
 
-    // 将 API 的响应转发给前端
-    res.status(response.status).json(response.data);
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+app.post("/call", async (req, res) => {
+    try {
+        const question = req.body.question;
+        const apiUrl =
+            "https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation";
+        const requestData = {
+            model: "qwen-v1",
+            input: {
+                prompt: question,
+            },
+            parameters: {},
+        };
+
+        const response = await axios.post(apiUrl, requestData, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${bearerToken}`,
+                // 如果需要在请求头中添加其他信息，可以在这里添加
+            },
+        });
+
+        // 将 API 的响应转发给前端
+        res.status(response.status).json({'code':200,'data':response.data.output});
+    } catch (error) {
+        console.error("Error:", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
--- proxy-backend-js/utils/yaml.js
+++ proxy-backend-js/utils/yaml.js
@@ -4,21 +4,12 @@ const yaml = require('js-yaml');
 function readYamlFile(filePath) {
   try {
     const fileContents = fs.readFileSync(filePath, 'utf8');
-    const yamlData = yaml.safeLoad(fileContents);
-    return yamlData;
+    const yamlData = yaml.load(fileContents);
+    return yamlData || {};
   } catch (error) {
     console.error(`Error reading YAML file: ${error.message}`);
-    return null;
+    return {};
   }
 }
 
-// 示例使用
-const yamlFilePath = 'path/to/your/file.yaml';
-const yamlData = readYamlFile(yamlFilePath);
-
-if (yamlData) {
-  // 现在，yamlData 包含了从 YAML 文件中读取的内容
-  console.log(yamlData);
-} else {
-  console.error('Failed to read YAML file.');
-}
+module.exports = { readYamlFile };
