dc0c8f3e593c829342c8089ddcff00fee65f3775
Lightsake-Sensei <805935229@qq.com>
2023-12-26 20:03:21 +0800

    更新文档

--- xinfan-observation-station/src/App.vue
+++ xinfan-observation-station/src/App.vue
@@ -60,7 +60,10 @@
 
     </div>
   </div> -->
-  <router-view></router-view>
+  <transition name="fade" mode="out-in" enter-active-class="animate__animated animate__fadeIn"
+    leave-active-class="animate__animated animate__fadeOut">
+    <router-view></router-view>
+  </transition>
 </template>
 
 <script>
@@ -293,4 +296,5 @@ export default {
   align-items: center;
   flex-wrap: wrap;
   padding: 30px;
-}</style>
+}
+</style>
